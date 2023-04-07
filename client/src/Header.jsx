536dd00f281bcd0c90458902162fe7787f210bc9
Reem <reembsrat@gmail.com>
2023-04-07 12:19:20 +0300

    added js-cookie in client

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -9,6 +9,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
 import "./styles/header.css";
 import { API_URL } from "./constants";
+import Cookies from 'js-cookie';
+
 
 const Header = ({
   cartItems,
@@ -19,6 +21,8 @@ const Header = ({
   const { setUserInfo, userInfo } = useContext(UserContext);
   const [isMobile, setIsMobile] = useState(false);
   const [isProfileFetched, setIsProfileFetched] = useState(false);
+  const jwtToken = Cookies.get('token');
+
 
   const navigate = useNavigate();
 
@@ -28,6 +32,9 @@ const Header = ({
     } else {
       fetch(`${API_URL}/profile`, {
         credentials: "include",
+        headers: {
+          Authorization: `Bearer ${jwtToken}`
+        }
       })
         .then((response) => {
           if (response.ok) {
