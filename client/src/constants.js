b1958fa4d14a2fa371a7454f75241eeed75bf6bb
Ree-m <reembsrat@gmail.com>
2023-07-27 04:45:22 +0300

    trying to fix build error

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,5 +1,2 @@
-// export const API_URL = import.meta.env.VITE_REACT_APP_API_URL;
+export const API_URL = import.meta.env.VITE_REACT_APP_API_URL;
 
-const { VITE_REACT_APP_API_URL } = import.meta.env;
-
-export const API_URL = VITE_REACT_APP_API_URL;
